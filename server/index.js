8899a39309b5d59812676171eea4e7fce70b3815
Joseph Carey <josephcareycomposer@gmail.com>
2019-10-26 10:23:13 -0500

    add a basic get route

--- server/index.js
+++ server/index.js
@@ -1,1 +1,24 @@
+const express = require("express");
+
+const app = express();
+const bodyParser = require("body-parser");
+
+// Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Route includes
+const apiRouter = require("./routes/api.router");
+
 console.log("Successfully Loaded.");
+
+/* Routes */
+app.use("/api/", apiRouter);
+
+// App Set //
+const PORT = process.env.PORT || 5001;
+
+/** Listen * */
+app.listen(PORT, () => {
+  console.log(`Listening on port: ${PORT}`);
+});
